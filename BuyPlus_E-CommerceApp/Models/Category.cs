fd397094659bfd2b6d2fec0491b642ef58a71fb8
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-08-29 00:16:08 +0300

    Created Category Model.

---
+++ BuyPlus_E-CommerceApp/Models/Category.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BuyPlus_E_CommerceApp.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public int DisplayOrder { get; set; }
+
+    }
+}
