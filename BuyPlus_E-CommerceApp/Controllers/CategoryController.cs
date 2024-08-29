c3dbb9df3f4da9057cec3599a4a4532b5497bab8
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-08-29 18:01:28 +0300

    Getting all Categories.Category View and Category Controller updated.

--- BuyPlus_E-CommerceApp/Controllers/CategoryController.cs
+++ BuyPlus_E-CommerceApp/Controllers/CategoryController.cs
@@ -1,12 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BuyPlus_E_CommerceApp.Data;
+using BuyPlus_E_CommerceApp.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace BuyPlus_E_CommerceApp.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public CategoryController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
-            return View();
+            List<Category> categoryList= _context.Categories.ToList();
+            return View(categoryList);
         }
     }
 }
