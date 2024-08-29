0c9856202af8f18c36ff3a90af706dfa790aef9d
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-08-29 14:15:03 +0300

    ApplicationDbContext added and configurations done.

---
+++ BuyPlus_E-CommerceApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BuyPlus_E_CommerceApp.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+    }
+}
