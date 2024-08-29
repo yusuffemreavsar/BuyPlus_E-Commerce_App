5de419e3bdf732ff2aac11978e6839d7b20173fc
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-08-29 14:55:46 +0300

    CategoryController created and Category View added.

---
+++ BuyPlus_E-CommerceApp/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BuyPlus_E_CommerceApp.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
