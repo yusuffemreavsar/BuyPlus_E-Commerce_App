a56662ca389220206471437117e2de43c13c688e
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-08-31 18:21:42 +0300

    Category Update Methods added.

--- BuyPlus_E-CommerceApp/Controllers/CategoryController.cs
+++ BuyPlus_E-CommerceApp/Controllers/CategoryController.cs
@@ -29,8 +29,28 @@ namespace BuyPlus_E_CommerceApp.Controllers
                _context.SaveChanges();
                return RedirectToAction("Index");
             }
-           
-
+            return View();
+        }
+        public IActionResult Edit(int id)
+        {
+            if(id ==null || id == 0)
+            {
+                return NotFound();
+            }
+            Category category = _context.Categories.FirstOrDefault(c => c.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+            return View(category);
+        }
+        [HttpPost]
+        public IActionResult Edit(Category category)
+        {
+            _context.Update(category);
+            _context.SaveChanges();
+            return RedirectToAction("Index");
+        
             return View();
         }
     }
