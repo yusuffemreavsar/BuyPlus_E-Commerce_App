ea3827b0df74bb7b2a76a79ca2c4aedb60b49826
yusuffemreavsar@gmail.com <yusuffemreavsar@gmail.com>
2024-09-01 13:05:26 +0300

    Razor Project's Entity Framework Configurations done and Category Migrations added..

--- BuyPlusRazor/Program.cs
+++ BuyPlusRazor/Program.cs
@@ -1,8 +1,13 @@
+using BuyPlusRazor.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+
+builder.Services.AddDbContext<ApplicationDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("BaseDb")));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
